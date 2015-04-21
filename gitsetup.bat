20674e9437b659b5110cb00a244e8cfa115f95bc
Rick Bates <rickbates@frontier.com>
2015-04-21 12:38:04 -0700

    initial project setup

---
+++ gitsetup.bat
@@ -0,0 +1,6 @@
+git init	
+git config --global user.name "Rick Bates"
+git config --global user.email rickbates@frontier.com
+git add *.*
+git add README
+git commit -m 'initial project setup'
---
+++ setup.bat
@@ -0,0 +1,16 @@
+
+md js
+md js\lib
+md css
+md views
+md images
+md ctrl
+
+call bower install jquery
+call bower install underscore
+call bower install backbone
+call bower install modernizer
+call bower install normalize
+call bower install bootstrap
+
+
